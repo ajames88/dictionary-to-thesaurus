d895fb40b2b0ec25ce74ab568108512422333d14
Austin James <austinjames49@gmail.com>
2019-04-23 00:37:17 -0400

    Created vectors for vector space model

--- CreateMyThesaurus.py
+++ CreateMyThesaurus.py
@@ -5,6 +5,17 @@
 # order to run this program.
 
 import sys
+import math
+
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+# Initialize the vector space list and the default vector ---------------------
+
+vectorDimensions = []
+vectors = []
+
+# -----------------------------------------------------------------------------
 
 # Read in the list of words and definitions for which to make a thesarus ------
 
@@ -15,19 +26,98 @@ dictionaryInputFile = "./ParsedDictionary/"+letter.upper()+".txt"
 read = open(dictionaryInputFile, "r")
 
 dictionary = []
+wordList = []
 
 for x in read:
     if x.count("TERM") > 0:
         endOfTerm = x.index("DEF")
         term = x[5:endOfTerm-1]
         definition = x[endOfTerm+4:(len(x)-1)].split()
+
         entry = []
         entry.append(term)
         entry.append(definition)
-        dictionary.append(entry)
+
+        for y in definition:
+            wordList.append(y.upper())
+            if y.upper() not in vectorDimensions:
+                vectorDimensions.append(y.upper())
+
+        if len(definition) > 0:
+            dictionary.append(entry)
 
 # The terms we are making a thesaurus for and their definitions are now in the
 # list dictionary -------------------------------------------------------------
+
+# Find the frequency with which each word appears in the dictionary -----------
+
+dictionaryFreqs = []
+
+for y in vectorDimensions:
+    dictionaryFreqs.append([y,0])
+
+for x in wordList:
+    i = vectorDimensions.index(x)
+    dictionaryFreqs[i][1] = dictionaryFreqs[i][1] + 1
+
+idfValues = []
+
+for x in dictionaryFreqs:
+    idfValues.append([x[0],(1+math.log10((len(dictionary)/x[1])))])
+
+# -----------------------------------------------------------------------------
+
+# Create the list of vectors for each term in the dictionary ------------------
+
+for x in range(len(dictionary)):
+
+    defaultVector = []
+    for y in vectorDimensions:
+        defaultVector.append([y,0])
+
+    for y in dictionary[x][1]:
+        i = vectorDimensions.index(y.upper())
+        defaultVector[i][1] = defaultVector[i][1] + 1
+        vectors.append(defaultVector)
+
+print("\nFreqs\n")
+print(vectors[0])
+# -----------------------------------------------------------------------------
+
+# Multiply each term in each vector by its idf value --------------------------
+
+dictionaryWords = []
+
+for x in dictionaryFreqs:
+    dictionaryWords.append(x[0])
+
+tfidfVectors = []
+
+for x in vectors:
+    vec = []
+    for y in x:
+        i = dictionaryWords.index(y[0])
+        temp = y[1]*idfValues[i][1]
+        vec.append([y[0], temp])
+    tfidfVectors.append(vec)
+
+
+print("\nFreqs with idfs\n")
+print(tfidfVectors[0])
+# -----------------------------------------------------------------------------
+
+# Normalize all the vectors ---------------------------------------------------
+
+for x in vectors:
+    sum = 0.0
+    for y in x:
+        sum += y[1]*y[1]
+    normfactor = math.sqrt(sum)
+    normfactor = 1/normfactor
+    for y in range(len(x)):
+        x[y][1] = float(x[y][1])*normfactor
+
+# -----------------------------------------------------------------------------
 
 # Writer to write thesaurus to output file ------------------------------------
 
@@ -38,9 +128,6 @@ writer = open(outputFile, "w")
 # -----------------------------------------------------------------------------
 
 # Compare against all definitions to find synonyms ----------------------------
-
-alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 synonyms = []
 
