2d7b66678e7661dba88ce6faf34b7aea1fc5a48a
Austin James <austinjames49@gmail.com>
2019-04-23 22:10:37 -0400

    Final Submission

--- CreateMyThesaurus.py
+++ CreateMyThesaurus.py
@@ -4,8 +4,11 @@
 # The program 'ParseThesarus.py' and 'ParsedDictionary.py' must be run in
 # order to run this program.
 
+print("\n")
+
 import sys
 import math
+import timeit
 
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -15,6 +18,38 @@ alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 vectorDimensions = []
 vectors = []
 
+numDefinitions = 0
+
+wordList = []
+
+comparisonDictionary = []
+comparisonVectors = []
+
+start = timeit.default_timer()
+
+for x in alphabet:
+    dictionaryFile = "./ParsedDictionary/"+x+".txt"
+    read = open(dictionaryFile)
+    for y in read:
+        if y.count("TERM") > 0:
+            numDefinitions = numDefinitions + 1
+            endOfTerm = y.index("DEF")
+            term = y[5:(endOfTerm-1)]
+            definition = y[endOfTerm+4:(len(y)-1)].split()
+            for z in definition:
+                wordList.append(z)
+            if len(definition) > 0:
+                entry = []
+                entry.append(term)
+                entry.append(definition)
+                comparisonDictionary.append(entry)
+
+vectorDimensions = list(dict.fromkeys(wordList))
+
+stop = timeit.default_timer()
+time = stop - start
+
+print("Vector Dimesnions computed (" + str(time) + " s)")
 # -----------------------------------------------------------------------------
 
 # Read in the list of words and definitions for which to make a thesarus ------
@@ -26,7 +61,8 @@ dictionaryInputFile = "./ParsedDictionary/"+letter.upper()+".txt"
 read = open(dictionaryInputFile, "r")
 
 dictionary = []
-wordList = []
+
+start = timeit.default_timer()
 
 for x in read:
     if x.count("TERM") > 0:
@@ -38,84 +74,142 @@ for x in read:
         entry.append(term)
         entry.append(definition)
 
-        for y in definition:
-            wordList.append(y.upper())
-            if y.upper() not in vectorDimensions:
-                vectorDimensions.append(y.upper())
-
         if len(definition) > 0:
             dictionary.append(entry)
+
+stop = timeit.default_timer()
+time = stop - start
+
+print(letter + " dictionary loaded (" + str(time) + " s)")
 
 # The terms we are making a thesaurus for and their definitions are now in the
 # list dictionary -------------------------------------------------------------
 
 # Find the frequency with which each word appears in the dictionary -----------
 
-dictionaryFreqs = []
+start = timeit.default_timer()
 
-for y in vectorDimensions:
-    dictionaryFreqs.append([y,0])
+dictionaryFreqs = {}
 
 for x in wordList:
-    i = vectorDimensions.index(x)
-    dictionaryFreqs[i][1] = dictionaryFreqs[i][1] + 1
+    if x.upper() not in dictionaryFreqs:
+        dictionaryFreqs[x.upper()] = 1
+    else:
+        dictionaryFreqs[x.upper()] = dictionaryFreqs[x.upper()] + 1
 
-idfValues = []
+stop = timeit.default_timer()
+time = stop - start
 
-for x in dictionaryFreqs:
-    idfValues.append([x[0],(1+math.log10((len(dictionary)/x[1])))])
+print("Word Frequencies found (" + str(time) + " s)")
+
+start = timeit.default_timer()
+
+idfValues = {}
+
+dividend = ( 1 + math.log10(numDefinitions) )
+
+for x, y in dictionaryFreqs.items():
+    idfValues[x] = dividend/y
+
+stop = timeit.default_timer()
+time = stop - start
+
+print("IDF Values computed (" + str(time) + " s)")
 
 # -----------------------------------------------------------------------------
 
 # Create the list of vectors for each term in the dictionary ------------------
 
+start = timeit.default_timer()
+
 for x in range(len(dictionary)):
 
-    defaultVector = []
-    for y in vectorDimensions:
-        defaultVector.append([y,0])
+    vec = {}
 
-    for y in dictionary[x][1]:
-        i = vectorDimensions.index(y.upper())
-        defaultVector[i][1] = defaultVector[i][1] + 1
-        vectors.append(defaultVector)
+    item = dictionary[x]
 
-print("\nFreqs\n")
-print(vectors[0])
-# -----------------------------------------------------------------------------
+    for y in item[1]:
+        if y.upper() in vec:
+            vec[y.upper()] = vec[y.upper()] + (1 * idfValues[y.upper()])
+        else:
+            vec[y.upper()] = (1 * idfValues[y.upper()])
 
-# Multiply each term in each vector by its idf value --------------------------
+    vectors.append([item[0],vec])
 
-dictionaryWords = []
+for x in range(len(comparisonDictionary)):
 
-for x in dictionaryFreqs:
-    dictionaryWords.append(x[0])
+    defaultVector = {}
 
-tfidfVectors = []
+    vec = {}
 
-for x in vectors:
-    vec = []
-    for y in x:
-        i = dictionaryWords.index(y[0])
-        temp = y[1]*idfValues[i][1]
-        vec.append([y[0], temp])
-    tfidfVectors.append(vec)
+    item = comparisonDictionary[x]
 
+    for y in item[1]:
+        if y.upper() in vec:
+            vec[y.upper()] = vec[y.upper()] + (1 * idfValues[y.upper()])
+        else:
+            vec[y.upper()] = (1 * idfValues[y.upper()])
 
-print("\nFreqs with idfs\n")
-print(tfidfVectors[0])
+    comparisonVectors.append([item[0],vec])
+
+stop = timeit.default_timer()
+time = stop - start
+print("Frequency vectors created (" + str(time) + " s)")
+
 # -----------------------------------------------------------------------------
 
 # Normalize all the vectors ---------------------------------------------------
 
+start = timeit.default_timer()
+
 for x in vectors:
     sum = 0.0
-    for y in x:
-        sum += y[1]*y[1]
-    normfactor = math.sqrt(sum)
-    normfactor = 1/normfactor
-    for y in range(len(x)):
-        x[y][1] = float(x[y][1])*normfactor
+    for y, z in x[1].items():
+        sum = (sum + (z * z))
+    root = math.sqrt(sum)
+    normfactor = (float(1) / root)
+    for y, z in x[1].items():
+        x[1][y] = (float(x[1][y]) * normfactor)
+
+for x in comparisonVectors:
+    sum = 0.0
+    for y, z in x[1].items():
+        sum = (sum + (z * z))
+    root = math.sqrt(sum)
+    normfactor = (float(1) / root)
+    for y, z in x[1].items():
+        x[1][y] = (float(x[1][y]) * normfactor)
+
+stop = timeit.default_timer()
+time = stop - start
+print("Normalized vectors (" + str(time) + " s)")
+
+# -----------------------------------------------------------------------------
+
+# Compute cosine similarities of all documents to all other documents ---------
+
+start = timeit.default_timer()
+
+synonyms = {}
+
+for x in dictionary:
+    synonyms[x[0]] = {}
+
+acceptableSimilarity = 0.4
+identicalSimilarity = 0.95
+
+for x in vectors:
+    for y in comparisonVectors:
+        similarity = 0.0
+        for t, v in x[1].items():
+            if t in y[1]:
+                similarity = (similarity + (v * y[1][t]))
+        if (similarity >= acceptableSimilarity) and (similarity < identicalSimilarity):
+            synonyms[x[0]][y[0]] = similarity
+
+stop = timeit.default_timer()
+time = stop - start
+print("Computed cosine similarity and found synonyms (" + str(time) + " s)")
 
 # -----------------------------------------------------------------------------
 
@@ -127,48 +221,21 @@ writer = open(outputFile, "w")
 
 # -----------------------------------------------------------------------------
 
-# Compare against all definitions to find synonyms ----------------------------
-
-synonyms = []
-
-for x in dictionary:
-    synonyms.append([x[0]])
-
-for x in alphabet:
-    dictionaryFile = "./ParsedDictionary/"+x+".txt"
-    read = open(dictionaryFile)
-    comparisonDictionary = []
-    for x in read:
-        if x.count("TERM") > 0:
-            endOfTerm = x.index("DEF")
-            term = x[5:(endOfTerm-1)]
-            definition = x[endOfTerm+4:(len(x)-1)].split()
-            entry = []
-            entry.append(term)
-            entry.append(definition)
-            comparisonDictionary.append(entry)
-    for x in range(len(dictionary)):
-        term = dictionary[x][0]
-        definition = dictionary[x][1]
-        entry = [term]
-        for y in comparisonDictionary:
-            similarWords = 0
-            comparisonTerm = y[0]
-            comparisonDefinition = y[1]
-            for z in definition:
-                if comparisonDefinition.count(z) > 0:
-                    similarWords = similarWords + 1
-            if similarWords > (len(definition)/2) and (comparisonTerm != term):
-                synonyms[x].append(comparisonTerm)
-
-# -----------------------------------------------------------------------------
-
 # Print to Output -------------------------------------------------------------
 
-for x in synonyms:
-    writer.write("TERM "+str(x[0]).upper()+" SYNONYMS")
-    for y in range(len(x)-1):
-        writer.write(" "+str(x[y+1]).upper())
-    writer.write("\n")
+start = timeit.default_timer()
+
+for x in dictionary:
+    if len(synonyms[x[0]]) > 0:
+        writer.write("TERM "+str(x[0]).upper()+" SYNONYMS")
+        for y in synonyms[x[0]]:
+            writer.write(" "+str(y.upper()))
+        writer.write("\n")
+
+stop = timeit.default_timer()
+time = stop - start
+print("Thesaurus written to output (" + str(time) + " s)")
 
 # -----------------------------------------------------------------------------
+
+print("\n")
